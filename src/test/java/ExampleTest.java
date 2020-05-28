9195e7a7b24a940c109c25f9d77003146be0a644
Henrik Fengler <henrik.fengler@klosebrothers.de>
2020-05-28 16:09:49 +0200

    initial commit

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+  @Test
+  public void thisIsTrue(){
+    Assertions.assertTrue(true);
+  }
+}
